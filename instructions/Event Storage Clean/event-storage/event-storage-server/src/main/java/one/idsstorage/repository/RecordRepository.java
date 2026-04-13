e3e7804fb9f2771bbc11411dc37c8c5c78ac2d7b
IDante <maxim.polyanskiy@mail.ru>
2026-04-13 20:16:06 +0300

    Remove accidentally committed instruction dump.
    Drop the copied 'instructions/Event Storage Clean' tree and related temporary instruction documents that were added by mistake.

--- instructions/Event Storage Clean/event-storage/event-storage-server/src/main/java/one/idsstorage/repository/RecordRepository.java
+++
@@ -1,9 +0,0 @@
-package one.idsstorage.repository;
-
-import one.idsstorage.domain.Record;
-
-import java.util.List;
-
-public interface RecordRepository {
-    void saveAll(List<Record> records);
-}
